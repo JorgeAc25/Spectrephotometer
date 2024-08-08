e752d585e4ed322d570df74493689f2157ef6bb6
JorgeAc25 <76083111+JorgeAc25@users.noreply.github.com>
2024-08-08 10:18:14 -0600

    Delete Spectrephotometer directory

--- Spectrephotometer/Espectrofotometro.py
+++
@@ -1,9 +0,0 @@
-from vistas.navegador import *
-
-if __name__ == "__main__":
-    ventana = Tk()
-    app = App(ventana)
-    ventana.mainloop()
-
-
-
--- Spectrephotometer/Espectrofotómetro.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\Jed\AppData\Local\Programs\Thonny\python.exe" "C:\Users\Jed\Desktop\Espectrofotometro\Espectrofotometro.py"
-pause
